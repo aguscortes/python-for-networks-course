1142a36f63730ca9722f1182851716c682f1b4e2
root <root@bacterio.localdomain>
2019-04-07 10:56:29 +0200

    mac_changer.py script good enough

--- 01-mac-changer/mac_changer.py
+++ 01-mac-changer/mac_changer.py
@@ -1,6 +1,7 @@
 #!/bin/python
 import subprocess
 import optparse
+import re
 
 def get_arguments():
 	parser = optparse.OptionParser()
@@ -19,10 +20,22 @@ def change_mac(interface, new_mac):
 	subprocess.call(['ifconfig', interface, 'hw', 'ether',new_mac])
 	subprocess.call(['ifconfig', interface, 'up'])
 
-def validate_mac(interface, new_mac):
-	subprocess.call(['ifconfig', '|' 'grep','-A1', interface, '|', 'tail','-1'])
-
+def get_current_mac(interface):
+ 	ifconfig_result = subprocess.check_output(['ifconfig', interface])
+	mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+	if mac_address_search_result:
+		return mac_address_search_result.group(0)
+	else:
+		print("Sorry, couldn't read the MAC Address")
 
 options = get_arguments()
+current_mac = get_current_mac(options.interface)
+print ("Current MAC: " + str(current_mac))
+
 change_mac(options.interface, options.new_mac)
-validate_mac(options.interface, options.new_mac)
+
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+	print ("[-] MAC Address was succesfully changed to " + current_mac )
+else:
+	print ("[-] MAC Address did not get changed")
